4b4f05d1e6b6b4ca1855dc909aafdc70a980b13f
Sandro <sandro88zgz@gmail.com>
2026-01-22 15:29:55 +0100

    ultimo guardado

--- src/main/java/com/sandro/gestionpeliculas/SplashController.java
+++ src/main/java/com/sandro/gestionpeliculas/SplashController.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.MissingResourceException;
